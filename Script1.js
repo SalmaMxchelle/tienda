6f084067f5e1a893b18497b3e30d164c75a3078e
Mich <salmamichellelopez@gmail.com>
2026-08-31 10:20:56 -0600

    Ignorando archivos

--- Script1.js
+++ Script1.js
@@ -4,3 +4,5 @@ let numero2 = 3;
 let suma = numero1 + numero2;
 
 console.log("La suma es:", suma);
+
+let arrayPets = ["gato", "perro", "caballo"];
---
+++ Script2.js
@@ -0,0 +1,15 @@
+console.log("Mensaje 1 ");
+
+function areaTriangulo(base, altura) {
+    let area = base * altura / 2
+    console.log("El area es " + area)
+
+}
+
+areaTriangulo(6, 7);
+
+console.log("Mensaje 2 ");
+function saludar(msg) {
+
+    console.log(msg);
+}
